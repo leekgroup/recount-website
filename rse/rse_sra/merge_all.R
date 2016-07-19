6f32072be4086bbc43800f47cbe3def0563dd046
lcolladotor <lcollado@jhsph.edu>
2016-07-19 09:06:25 -0400

    Replace last cbind by manual code

--- rse/rse_sra/merge_all.R
+++ rse/rse_sra/merge_all.R
@@ -55,17 +55,58 @@ files_main <- function(type) {
         load(rse_list_file)
     }
     
-    message(paste(Sys.time(), 'merging RSE objects by sets of groups at the',
-        type, 'level'))
-    group.sets <- cut2(seq_len(length(rse_list)), m = 5)
-    rse_list_sets <- lapply(levels(group.sets), function(group) {
-        do.call(cbind, rse_list[group.sets == group])
-    })
+    rse_list_sets_file <- paste0(
+        '/dcl01/leek/data/recount-website/rse/rse_sra/all/rse_list_sets_',
+        type, '.Rdata')
+    if(!file.exists(rse_list_sets_file)) {
+        message(paste(Sys.time(),
+            'merging RSE objects by sets of groups at the', type, 'level'))
+        group.sets <- cut2(seq_len(length(rse_list)), m = 5)
+        rse_list_sets <- lapply(levels(group.sets), function(group) {
+            do.call(cbind, rse_list[group.sets == group])
+        })
+    } else {
+        message(paste(Sys.time(),
+            'loading previously computed rse_list_sets at the', type, 'level'))
+        load(rse_list_sets_file)
+    }
     
     message(paste(Sys.time(), 'merging RSE objects from sets at the', type,
         'level'))
+    
+
+    ## Extract data from sets
+    message(paste(Sys.time(), 'extracting information from sets'))
+    col_data <- do.call(rbind, lapply(rse_list_sets, colData))
+    row_ranges <- rowRanges(rse_list_sets[[1]])
+    counts_list <- lapply(rse_list_sets, function(x) assays(x)$counts)
+    
+    ## Prepare counts matrix
+    message(paste(Sys.time(), 'preparing the counts matrix'))
+    counts_n <- sapply(counts_list, ncol)
+    counts_adj <- c(0, cumsum(counts_n[1:(length(counts_n) - 1)]))
+    counts_idx <- mapply(function(n, adj) { seq_len(n) + adj}, counts_n,
+        counts_adj, SIMPLIFY = FALSE)
+    
+    ## Initialize count matrix
+    message(paste(Sys.time(), 'initializing counts matrix'))
+    counts <- matrix(0, nrow = nrow(counts_list[[1]]),
+        ncol = sum(counts_n), dimnames = list(rownames(counts_list[[1]]),
+        unlist(lapply(counts_list, colnames))))
+    
+    ## Fill in matrix
+    for(i in seq_len(length(counts_idx))) {
+        message(paste(Sys.time(), 'filling in counts matrix with set', i))
+        counts[, counts_idx[[i]]] <- counts_list[[i]]
+    }
+    
+    message(paste(Sys.time(), 'creating final rse object'))
     varname <- paste0('rse_', type)
-    assign(varname, do.call(cbind, rse_list_sets))
+    assign(varname,
+        SummarizedExperiment(assays = list('counts' = counts),
+            rowRanges = row_ranges, colData = col_data)
+    )
+
     
     ## Save results
     rse_file <- paste0('/dcl01/leek/data/recount-website/rse/rse_sra/all/rse_',
@@ -74,11 +115,20 @@ files_main <- function(type) {
     return(rse_file)
 }
 
-message(paste(Sys.time(), 'processing gene files'))
-files_main('gene')
+if(!file.exists('rse_gene.Rdata')) {
+    message(paste(Sys.time(), 'processing gene files'))
+    files_main('gene')
+} else {
+    message(paste(Sys.time(), 'rse_gene.Rdata already exists'))
+}
 
-message(paste(Sys.time(), 'processing exon files'))
-files_main('exon')
+
+if(!file.exists('rse_exon.Rdata')) {
+    message(paste(Sys.time(), 'processing exon files'))
+    files_main('exon')
+} else {
+    message(paste(Sys.time(), 'rse_exon.Rdata already exists'))
+}
 
 ## Junctions won't work asis since not all the rows are the same, plus it'd be huge
 #message(paste(Sys.time(), 'processing exon files'))
