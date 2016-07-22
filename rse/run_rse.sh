ff85eb8c504702a550fdd340b6a879c4902628ce
lcolladotor <lcollado@jhsph.edu>
2016-07-22 00:21:29 -0400

    Run twice as many jobs simultaneously

--- rse/run_rse.sh
+++ rse/run_rse.sh
@@ -20,7 +20,7 @@ mkdir -p ${WDIR}/logs
 if [[ "${PROJECT}" == "sra" ]]
 then
     echo "$PROJECT"
-    MEM="mem_free=100G,h_vmem=160G,h_fsize=50G"
+    MEM="mem_free=50G,h_vmem=160G,h_fsize=50G"
 elif [[ "${PROJECT}" == "gtex" ]]
 then
     echo "$PROJECT"
