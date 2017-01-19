dae67e40a7c45a64c59093de796ca0d6fb9115d3
lcolladotor <lcollado@jhsph.edu>
2017-01-19 00:16:38 -0500

    Fix a typo

--- rse/submit_extra.sh
+++ rse/submit_extra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /dcl01/leek/data/recount-website/rse/rse_sra
-qsub run_merse.sh
+qsub run_merge.sh
 cd /dcl01/leek/data/recount-website/rse/rse_gtex
 qsub run_split.sh
 cd /dcl01/leek/data/recount-website/rse/rse_tcga
